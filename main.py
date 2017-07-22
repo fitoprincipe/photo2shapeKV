9aaaeeef757529b0b0b13acde09794f87588b1f8
Rodrigo <fitoprincipe82@gmail.com>
2017-07-22 16:23:38 -0300

    added a boxlayout to make a list of selected files.

--- main.py
+++ main.py
@@ -3,14 +3,45 @@
 
 from kivy.app import App
 from kivy.uix.filechooser import FileChooserListView
-from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.properties import (ObjectProperty, ListProperty,
+                             StringProperty)
 
 import exifread
 
 
 class Files(FileChooserListView):
+
+    selectedFiles = ListProperty([])
+
     def __init__(self, **kwargs):
         super(Files, self).__init__(**kwargs)
+
+    def add2list(self, selection, touch):
+        pass
+
+
+class SelectedFilesRow(BoxLayout):
+    color = ListProperty([])
+    type = StringProperty()
+    text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(SelectedFilesRow, self).__init__(**kwargs)
+
+
+class SelectedFiles(BoxLayout):
+    selectedFiles = ListProperty([])
+
+    def __init__(self, **kwargs):
+        super(SelectedFiles, self).__init__(**kwargs)
+        self.orientation = "vertical"
+
+    def _update(self):
+        lab = SelectedFilesRow(text=self.selectedFiles[-1])
+        self.add_widget(lab)
+
 
 class P2sApp(App):
     def build(self):
