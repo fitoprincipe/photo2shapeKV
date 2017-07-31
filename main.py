662c88501ea91c9fcc2d84009d7bc9e4f77f8a69
Rodrigo <rprincipe@ciefap.org.ar>
2017-07-31 10:46:32 -0300

    Revert "Merge branch 'master' of /home/labgeo1/repos/photo2shapeKV with conflicts."
    This reverts commit e555176

--- main.py
+++ main.py
@@ -3,36 +3,70 @@
 
 from kivy.app import App
 from kivy.uix.filechooser import FileChooserListView
-from kivy.properties import ObjectProperty, ListProperty
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
-from kivy.garden.filebrowser import FileBrowser
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import (ObjectProperty, ListProperty,
+                             StringProperty)
+from kivy.uix.gridlayout import GridLayout
 
 import exifread
+import shapefile
 
 
-class FilesScreen(Screen):
+class Menu(BoxLayout):
+    def __init__(self, **kwargs):
+        super(Menu, self).__init__(**kwargs)
+
+
+class MainWindow(BoxLayout):
     pass
 
 
-class AppScreen(Screen):
+class SubWindow(BoxLayout):
     pass
 
 
-class Files(FileBrowser):
+class Files(FileChooserListView):
+
+    # selectedFiles = ListProperty([])
+
     def __init__(self, **kwargs):
         super(Files, self).__init__(**kwargs)
 
+    def add2list(self, selection, touch):
+        pass
 
-class ScreenMan(ScreenManager):
+
+class SelectedFilesRow(BoxLayout):
+    """ This is the widget for each selected file. It has its path
+    (Label) and a cancell button at its right.
+    """
+    # TODO: wrap the path label
+    color = ListProperty([])
+    type = StringProperty()
+    text = StringProperty()
+
     def __init__(self, **kwargs):
-        super(ScreenMan, self).__init__(**kwargs)
-        self.transition = FadeTransition()
+        super(SelectedFilesRow, self).__init__(**kwargs)
+
+
+class SelectedFiles(GridLayout):
+    selectedFiles = ListProperty([])
+
+    def __init__(self, **kwargs):
+        super(SelectedFiles, self).__init__(**kwargs)
+
+    def _update(self):
+        self.clear_widgets()
+        for file in self.selectedFiles:
+            f = file
+            lab = SelectedFilesRow(text=f)
+            self.add_widget(lab)
 
 
 class P2sApp(App):
+    title = "Photo 2 Shape KV"
     def build(self):
-        sm = ScreenMan()
-        return sm
+        pass
 
     @staticmethod
     def tieneGPS(img):
@@ -47,7 +81,6 @@ class P2sApp(App):
         losTags = exifread.process_file(imagen)
 
         return True if 'GPS GPSLongitude' in losTags.keys() else False
-
 
 if __name__ == "__main__":
     P2sApp().run()
